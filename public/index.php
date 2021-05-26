e9d40abec35a48df2adccd33867a23eddc3a6a20
Yani <yani@xenokore.com>
2021-05-26 14:07:11 +0200

    move router functionality back to index.php

--- public/index.php
+++ public/index.php
@@ -6,6 +6,20 @@
  */
 
 require_once __DIR__ . '/../app/bootstrap.php';
-require_once __DIR__ . '/../app/router.php';
 
+/** @var Slim\App */
+$router = $app->getSlimRouter();
+
+// Add middlewares to Slim router
+foreach ((include __DIR__ . '/../app/middlewares.php') as $middleware_class) {
+    $router->add(
+        // Get from container so we can autowire dependencies
+        $container->get($middleware_class)
+    );
+}
+
+// Setup routes
+include __DIR__ . '/../app/routes.php';
+
+// Execute router
 $router->run();
